acc4ce0987350051740d442c0885fae24f8b8417
chaen <chaen4310@gmail.com>
2023-03-29 21:30:42 +0900

    2장 연습문제 ~8번

--- 2장 연습문제/practice.py
+++ 2장 연습문제/practice.py
@@ -29,3 +29,30 @@ print(first, last)
 pin = '881120-1068234'
 sex = pin[7]
 print(sex)
+
+# 5 문자열 바꾸기
+
+a = "a:b:c:d"
+
+print(a.replace(":", "#"))
+
+# 6 리스트 역순 정렬하기
+
+l = [1, 3, 5, 4, 2]
+
+print(l.sort())
+
+# 7 리스트를 문자열로 만들기
+
+str = ['life', 'is', 'too', 'short']
+
+print(" ".join(str))
+
+# print(*str)
+
+# 8 튜플 더하기
+
+tup = 1, 2, 3
+tup2 = (4,)
+tup3 = tup+tup2
+print(tup3)
